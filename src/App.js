b7af55fc90e4113653d91825aad9af82525cb141
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-15 16:04:39 +0100

    updates
    updates

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart } from './components';
+import { Products, Navbar, Cart, Checkout } from './components';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const App = () => {
@@ -69,6 +69,7 @@ const App = () => {
 							/>
 						}
 					/>
+					<Route path='/checkout' element={<Checkout />} />
 				</Routes>
 			</div>
 		</Router>
---
+++ src/components/CheckoutForm/Checkout/Checkout.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+import {
+	Paper,
+	Stepper,
+	Step,
+	StepLabel,
+	Typography,
+	CircularProgress,
+	Divider,
+	Button,
+} from '@material-ui/core';
+import { mergeClasses } from '@material-ui/styles';
+
+import useStyles from './styles';
+
+import AddressForm from '../AddressForm';
+import PaymentForm from '../PaymentForm';
+
+const steps = ['Shipping address', 'Payment details'];
+
+const Checkout = () => {
+	const classes = useStyles();
+	const [activeStep, setActiveStep] = useState(0);
+
+	const Confirmation = () => {
+		return <div>Confirmation</div>;
+	};
+
+	const Form = () => (activeStep === 0 ? <AddressForm /> : <PaymentForm />);
+
+	return (
+		<>
+			<div className={classes.toolbar} />
+			<main className={classes.layout}>
+				<Paper className={classes.paper}>
+					<Typography variant='h4' align='center'>
+						Checkout
+					</Typography>
+					<Stepper activeStep={0} className={classes.stepper}>
+						{steps.map((step) => (
+							<Step key={step}>
+								<StepLabel>{step}</StepLabel>
+							</Step>
+						))}
+					</Stepper>
+					{activeStep === steps.length ? <Confirmation /> : <Form />}
+				</Paper>
+			</main>
+		</>
+	);
+};
+
+export default Checkout;
