4623361563b20c3cdad93b794b955a2043201ec5
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-16 23:52:49 +0100

    Saving JSON Cart data file
    Saving JSON Cart data file

--- src/lib/saveToJson.js
+++ src/lib/saveToJson.js
@@ -4,7 +4,15 @@ const commerce = new Commerce(
 	'pk_test_358871ee6839c49ff467c487b783616975366ecc9b8f6',
 	true
 );
+const fs = require('fs');
 
-var obj;
-
-commerce.cart.retrieve().then((cart) => (obj = cart));
+commerce.cart
+	.retrieve()
+	.then(async (cart) => {
+		const someData = JSON.stringify(cart);
+		await fs.writeFile('data.json', someData, function (err) {
+			if (err) return console.log(err);
+			console.log('Success');
+		});
+	})
+	.catch((err) => console.log(err));
