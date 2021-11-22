15834ce26a2ee126d6f48efea3973defde8ee3a7
Zlatko Marjanović <40026726+zlatkomarjanovic@users.noreply.github.com>
2021-11-22 18:08:45 +0100

    partnering brands
    partnering brands

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Banner } from '..';
+import Brand from '../Brand/Brand';
 import Products from '../Products/Products';
 const Home = ({ products, onAddToCart }) => {
 	return (
 		<div>
 			<Banner />
+			<Brand />
 			<Products products={products} onAddToCart={onAddToCart} />
 		</div>
 	);
